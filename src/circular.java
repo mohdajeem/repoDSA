df2b94a504c37560bdf7cf7a83318a6cdff56e45
mohdajeem <mohdajeem2003@gmail.com>
2024-03-20 13:30:59 +0530

    checking commit

---
+++ src/circular.java
@@ -0,0 +1,65 @@
+public class circular{
+    public static class Node{
+        int data;
+        Node next;
+
+        public Node(int data){
+            this.data = data;
+            this.next = null;
+        }
+    }
+
+    public static boolean isCycle(){
+        Node slow = head;
+        Node fast = head;
+
+        while(fast !=null && fast.next != null){
+            slow = slow.next;
+            fast = fast.next.next;
+            if(slow == fast){
+                return true;
+            }
+        }
+        return false;
+    }
+    public static void removeCycle(){
+        // detect cycle
+        Node slow = head;
+        Node fast = head;
+        boolean cycle = false;
+        while(fast != null && fast.next != null){
+            slow = slow.next;
+            fast = fast.next.next;
+            if(fast == slow){
+                cycle = true;
+                break;
+            }
+        }
+        if(cycle == false){
+            return;
+        }
+        slow = head;
+        Node previous = head;//last node
+        while(slow != fast){
+            previous = fast;
+            slow = slow.next;
+            fast = fast.next;
+        }
+        // removing circular ll
+        previous.next = null;
+
+    }
+    public static Node head=null;
+    public static Node tail=null;
+
+    public static void main(String[] args){
+        head = new Node(1);
+        head.next = new Node(2);
+        head.next.next = new Node(3);
+        head.next.next.next = head;
+        System.out.println(isCycle());
+        removeCycle();
+        System.out.println(isCycle());
+
+    }
+}
